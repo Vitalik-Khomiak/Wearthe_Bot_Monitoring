95e898dfb19a891c3d107420ab4b700f72b4f303
ArtLab <vitaliikhomiakk@gmail.com>
2024-04-28 15:17:20 +0000

    five

---
+++ Weather_art_bot/config.py
@@ -0,0 +1,3 @@
+open_weather_token = "f27780e4ac12a5e1f9d8e9beb7952066"
+tg_bot_token = "6922757056:AAGZSmSRPg2Noqj4o61WBMf-2lhBC1iQkbo"
+openai_api_key = "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"
