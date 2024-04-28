3f5980cce71ffaa0402c332ffeeff0dd8c79c411
ArtLab <vitaliikhomiakk@gmail.com>
2024-04-28 20:59:37 +0000

    new

--- Weather_art_bot/config.py
+++ Weather_art_bot/config.py
@@ -2,5 +2,4 @@ from os import environ
 
 open_weather_token = environ.get("OPEN_WEATHER_TOKEN","define me")
 tg_bot_token = environ.get("TG_BOT_TOKEN","define me")
-# openai_api_key = environ.get("OPENAI_API_KEY","define me")
-openai_api_key = "sk-proj-UHwsFyPGeI389oit8gwjT3BlbkFJj2x4QI9OINfdOgcSPtNb"
+openai_api_key = environ.get("OPENAI_API_KEY","define me")
