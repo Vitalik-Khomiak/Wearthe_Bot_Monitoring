6b23049100f8fb4a7a58c5017eaebbc8a175ab34
ArtLab <vitaliikhomiakk@gmail.com>
2024-05-26 23:57:51 +0000

    afsdfsdfgsdfsdds

--- Monitoring/jenkins/credentials.groovy
+++ Monitoring/jenkins/credentials.groovy
@@ -3,6 +3,8 @@ import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.plugins.credentials.*
 import jenkins.model.*
 import hudson.util.Secret
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
+
 
 // Функція для отримання змінних середовища
 def getEnvVariable(String name) {
@@ -18,14 +20,15 @@ def openWeatherToken = getEnvVariable('OPEN_WEATHER_TOKEN')
 
 def domain = Domain.global()
 def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-def creds = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "dockerhub_token", "Description", dockerUserName, dockerPassword)
 
+def creds = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "dockerhub_token", "Description", dockerUserName, dockerPassword)
 store.addCredentials(domain, creds)
 
 def tgBotTokenCreds = new StringCredentialsImpl(CredentialsScope.GLOBAL, "tg_bot_token", "TG Bot Token", tgBotToken)
-def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[1].getStore()
 store.addCredentials(domain, tgBotTokenCreds)
+
 def openaiApiKeyCreds = new StringCredentialsImpl(CredentialsScope.GLOBAL, "openai_api_key", "OpenAI API Key", openaiApiKey)
 store.addCredentials(domain, openaiApiKeyCreds)
+
 def openWeatherTokenCreds = new StringCredentialsImpl(CredentialsScope.GLOBAL, "open_weather_token", "Open Weather Token", openWeatherToken)
 store.addCredentials(domain, openWeatherTokenCreds)
